30065e585d11e2baf00e8f4bad51c5246aed30c9
MoGang <mkm9542@inha.edu>
2025-02-18 11:17:42 +0900

    v7.1 append

--- day07.py
+++ day07.py
@@ -1,32 +1,25 @@
-class Stack:
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
     def __init__(self):
-        self.items = []
+        self.head = None
 
-    def push(self, data):
-        self.items.append(data) #삽입 오퍼레이션
+    def append(self, data): #헤드가 가르킨 노드가 있는지 없는지
+        if not self.head:
+            self.head = Node(data)
+            return
+        current = self.head
+        while current.next: #존재하면 연결돼있다는 뜻이니까
+            current = current.next #다음노드로 이동
+        current.next = Node(data)
 
-    def pop(self): #스택에서의 팝은 마지막값을 꺼내므로, 인자값 필요X
-        return self.items.pop()
 
-    def size(self):
-        return len(self.items)
+if __name__ == "__main__":
+    l = LinkedList()
+    l.append(7)
+    l.append(-11)
+    l.append(8)
 
-    def is_empty(self):     #def is_empty(self) -> bool:
-        return len(self.items) == 0
-
-    def peek(self):          #def peek(self) -> object:
-        return self.items[-1]
-
-s1 = Stack()
-s1.push(-9)
-s1.push(6)
-s1.push(33)
-
-# print(s1.pop()) #33, 삭제되고
-# print(s1.peek()) #6, 삭제 안 되므로
-# print(s1.peek()) #6
-
-print(s1.peek()) #33
-print(s1.pop()) #33
-print(s1.peek()) #6
-print(s1.is_empty()) #6과 -9가 남아있으므로, False
