c8f336cf5e23e8ae412ab6184cd091fe2b3b1e4f
MoGang <mkm9542@inha.edu>
2025-02-18 13:19:51 +0900

    v7.2 random test

--- day07.py
+++ day07.py
@@ -1,25 +1,23 @@
-class Node:
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
+import random
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def append(self, data): #헤드가 가르킨 노드가 있는지 없는지
-        if not self.head:
-            self.head = Node(data)
-            return
-        current = self.head
-        while current.next: #존재하면 연결돼있다는 뜻이니까
-            current = current.next #다음노드로 이동
-        current.next = Node(data)
+i = 0
+while i < 20:
+    n = random.randint(1, 20)
+    l.append(n)
+    print(n, end=' ')
+    i = i + 1
+# print(l)
+print(l.search(10))
 
 
-if __name__ == "__main__":
-    l = LinkedList()
-    l.append(7)
-    l.append(-11)
-    l.append(8)
+#또는
+import random
 
+for i in range(0,20):
+    n = random.randint(1,30)
+    l.append(n)
+    print(j, end=" ")
+
+print(l.search(10))
+
+    
